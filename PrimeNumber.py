f8834fff1152c7150ba7f088e42d201cf2cc2141
rikuhidaka <riku_0826_h@yahoo.ne.jp>
2020-01-24 06:59:43 +0900

    Update PrimeNumber.py
    バグ修正

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,10 +1,13 @@
 Prime_number = [2]
 current = 3
+judge = True
 while True:
     for num in Prime_number:
         if current%num == 0:
-            Prime_number.append(current)
-            if current > 10000:
-                print(current + '\n')
-            break
+            judge = False
+    if judge == True:
+        Prime_number.append(current)
+        if current > 10000:
+            print(current)
+    judge = True
     current+=1
